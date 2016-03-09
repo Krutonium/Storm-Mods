761d9ebd966287649f67e6c529bd0b861287ce70
Zoryn Aaron <zoryn4163@gmail.com>
2016-03-09 02:44:25 -0500

    forceshowconsole

---
+++ ForceShowConsole/ForceShowConsole/ForceShowConsole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Storm.ExternalEvent;
+using Storm.StardewValley.Event;
+
+namespace ForceShowConsole
+{
+    [Mod]
+    public class ForceShowConsole
+    {
+        [DllImport("kernel32")]
+        static extern bool AllocConsole();
+        public static Thread ConsoleThread;
+
+        public void Init(InitializeEvent @event)
+        {
+            AllocConsole();
+
+            Console.WriteLine("Console Shower by Zoryn => Initialization Completed");
+        }
+    }
+}
